91485f1500aa1c15251213fe995238921aff5893
alessandro <corbisoft@gmail.com>
2014-04-03 19:01:37 +0200

    code cleaning

--- torrent_migration_helper.py
+++ torrent_migration_helper.py
@@ -4,29 +4,25 @@ import torrentparse as tp
 import shutil
 
 DEFAULT_PATH = './torrentfile_source/'
+DEFAULT_F_EXT = '.torrent'
+
 
 def list_dir(path = DEFAULT_PATH 
                   , print_list = False
-                  , fending = '.torrent'
-             , just_files = True ):
+                  , fending = DEFAULT_F_EXT
+            , just_files = True ):
     
-    print "acquiring file list..."
-    #flist =  [] #glob.glob(path + '/*.torrent')
+    print "acquiring file list..."    
     if just_files:
         flist = [ (f,os.path.join(path,f)) for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) and f.endswith(fending) ]
     else:
         flist = [ (f,os.path.join(path,f)) for f in os.listdir(path) if f.endswith(fending) ]
-
     if print_list:
         for f,fp in flist:
             print fp
 
-    print "done"
-
-    
-    
+    print "done!"    
     return flist
-
 
 def build_df_from_flist(path
                         , flist = None                        
@@ -36,7 +32,6 @@ def build_df_from_flist(path
         flist = list_dir(path)
 
     flist_df = pd.DataFrame(flist, columns = ['fname','fpath'])
-
     flist_df['fname_plain'] = flist_df.fname.apply( lambda f : f[:-len(default_ext)])
 
     def parseTorrent(r):
@@ -47,11 +42,9 @@ def build_df_from_flist(path
         return ret
 
     print "parsing torrents..."
-
     flist_df['TorrentParser'] = flist_df.apply( parseTorrent , axis = 1)
     
     print "extracting trackers..."
-
     def get_tracker(r):
         if r is not None:
             try:
@@ -62,19 +55,15 @@ def build_df_from_flist(path
             return ''
 
     flist_df['TrackerUrl'] = flist_df.apply( get_tracker , axis = 1 )
-
     print "done!"
-
     
     return flist_df
 
 def parse_tracker_list(flist_df,substr):
+
     flist_flt = flist_df[flist_df.TrackerUrl.apply(lambda st : substr in st)]
-
     sorted_flist = flist_flt.fname
-
     sorted_flist.order().to_csv('output.csv')
-
     return flist_flt
 
 def gather_downloaded_files(path):
@@ -83,17 +72,14 @@ def gather_downloaded_files(path):
                      , just_files = False)
 
     flist_names_set = set([f for f,fp in flist])
-
     return {'fnames' : flist , 'fnames_set' : flist_names_set}
     
 def filter_torrent_in_given_dl_site(flist_df,dl_site_content):
+
     print "filtering for dl site..."
     matching = flist_df.fname_plain.apply(lambda fn : fn in dl_site_content['fnames_set'])
-
     print "done"
     return matching
-
-
 
 if __name__ == '__main__':
 
